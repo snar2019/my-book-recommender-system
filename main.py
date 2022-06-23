1dc8f6a28f157e4401f3a13896a08270291fe607
Smritee Narain <narain.narain21@gmail.com>
2022-06-23 12:00:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 
