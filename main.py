6f847a478f6093c16998431445ebd9e5b5822d1f
Smritee Narain <narain.narain21@gmail.com>
2022-06-23 12:03:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
 #import seaborn as sns
-from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 
 header = st.container()
